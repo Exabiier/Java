5c8cad7c28fe08def1beaa5654008ed7751e5cf9
Exabiier <exabiier.bars@gmail.com>
2022-10-04 17:38:03 -0500

    Starting Java

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -3,6 +3,6 @@ package Java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, Exabiier!");
+        System.out.println("Hello, Exabiieer!");
     }
 }

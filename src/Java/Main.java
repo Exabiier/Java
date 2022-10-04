a370d0004d9112ac4f000b583cfb5192d3b782ae
Exabiier <exabiier.bars@gmail.com>
2022-10-04 18:37:25 -0500

    Basic data types

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -2,7 +2,21 @@ package Java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, Exabiieer!");
+//        System.out.println("Hello, World!");
+//        System.out.println("Hello, Exabiieer!");
+
+//        primative data types:
+        int hello_world = 5;
+        double num2 = 5.00;
+        boolean b = true;
+//        or false
+        char c = 'e';
+//        char = '9';
+        String str = "Tim";
+//        String str = "9"
+
+
+        System.out.println(hello_world);
+
     }
 }
